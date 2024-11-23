ed0226a5d94bdf6c244a2629509662e99c29d156
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-11-23 10:07:30 -0300

    feat: ✨ improve home page view

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,23 +1,27 @@
-'use client'
 import { ProductCarousel } from '@/components/products/product-carousel'
 import { ProductList } from '@/components/products/product-list'
 
 export default function Home() {
   const products = Array.from({ length: 3 }).map((_, index) => {
     return {
-      name: `item-${index}`
+      name: `item-${index}`,
     }
   })
 
   const productsList = Array.from({ length: 12 }).map((_, index) => {
     return {
-      name: `item-${index}`
+      name: `item-${index}`,
     }
   })
 
-  return <div className='flex flex-col gap-5 gap-x-0 px-0'>
-    <h2>Produtos</h2>
-    <ProductCarousel products={products} />
-    <ProductList data={productsList} />
-  </div>
+  return (
+    <div className="flex flex-col gap-5 gap-x-0 px-0">
+      <h2 className="text-3xl">Produtos em destaque</h2>
+      <div className="flex w-full items-center justify-center">
+        <ProductCarousel products={products} />
+      </div>
+      <h2 className="text-3xl">Produtos</h2>
+      <ProductList data={productsList} />
+    </div>
+  )
 }
--- src/components/products/product-carousel.tsx
+++ src/components/products/product-carousel.tsx
@@ -1,45 +1,42 @@
-"use client"
+'use client'
 
-import { Card, CardContent } from "../ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
-import Autoplay from "embla-carousel-autoplay"
+import { Card, CardContent } from '../ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
 
-export function Example() {
+export function ProductCarousel({ products }: { products: any[] }) {
   return (
     <Carousel
+      opts={{
+        align: 'center',
+        loop: true,
+      }}
+      className="w-11/12"
       plugins={[
         Autoplay({
           delay: 2000,
         }),
       ]}
     >
-      // ...
+      <CarouselContent className="w-full">
+        {products.map((product, index) => (
+          <CarouselItem key={index}>
+            <Card className="flex h-72 w-full items-center justify-center">
+              <CardContent className="flex items-center justify-center">
+                <span className="text-3xl font-semibold">{product.name}</span>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
     </Carousel>
   )
-}
-
-
-export function ProductCarousel({ products }: { products: any[] }) {
-  return <Carousel
-    className="w-full"
-    plugins={[
-      Autoplay({
-        delay: 2000
-      })
-    ]}
-  >
-    <CarouselContent className="w-full">
-      {products.map((product, index) => (
-        <CarouselItem key={index}>
-          <Card className="w-full flex justify-center items-center h-40">
-            <CardContent className="flex items-center justify-center">
-              <span className="text-3xl font-semibold">{product.name}</span>
-            </CardContent>
-          </Card>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    <CarouselPrevious />
-    <CarouselNext />
-  </Carousel>
 }
