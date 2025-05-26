bef2cf6a04be33ec8b5d33df8ddf3b726e4849b4
Gustavo Bertonsin <gustavo.bertonsin@kingspanisoeste.com.br>
2025-05-26 12:36:09 -0300

    feat: ✨ add footer only to private screens

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth/auth-options'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 export default async function PrivateLayout({
   children,
@@ -13,5 +13,17 @@ export default async function PrivateLayout({
     redirect('/sign-in')
   }
 
-  return <div className="min-h-screen bg-background">{children}</div>
+  return <div className="min-h-screen bg-background">
+    {children}
+    <footer className="border-t">
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+            &copy; {new Date().getFullYear()} Colet. Todos os direitos
+            reservados.
+          </p>
+        </div>
+      </div>
+    </footer>
+  </div>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,16 +26,6 @@ export default function RootLayout({
             <motion.div animate={{ translateY: [-20, 0], opacity: [0, 1], transition: { duration: 0.5, delay: 0.2 } }} className="min-h-screen bg-background">
               <Navbar />
               <main className="flex-1">{children}</main>
-              <footer className="border-t">
-                <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-                  <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-                    <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                      &copy; {new Date().getFullYear()} Colet. Todos os direitos
-                      reservados.
-                    </p>
-                  </div>
-                </div>
-              </footer>
             </motion.div>
           </AnimatePresence>
         </Providers>
