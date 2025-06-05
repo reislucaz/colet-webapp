7654181938456c53abc3aca56b06df077c30919f
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2025-06-04 21:21:34 -0300

    add validation to allow offer creation for other users than the author

--- src/components/routes/product-details/offer-modal.tsx
+++ src/components/routes/product-details/offer-modal.tsx
@@ -1,34 +1,37 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Product } from "../../../@types/product";
-import { useOfferConstant } from "../../../hooks/use-offer-constant";
-import { ChatService } from "../../../services/chat-service";
-import { OfferService } from "../../../services/offer-service";
-import { createOfferSchema, CreateOfferType } from "../../../validations/create-offer-schema";
-import { FormRender } from "../../shared/form/form-field-dynamic/FormRender";
-import { Button } from "../../ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "../../ui/dialog";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useSession } from 'next-auth/react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Product } from '../../../@types/product'
+import { useOfferConstant } from '../../../hooks/use-offer-constant'
+import { ChatService } from '../../../services/chat-service'
+import { OfferService } from '../../../services/offer-service'
+import {
+  createOfferSchema,
+  CreateOfferType,
+} from '../../../validations/create-offer-schema'
+import { FormRender } from '../../shared/form/form-field-dynamic/FormRender'
+import { Button } from '../../ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '../../ui/dialog'
 
 export function OfferModal(product: Product) {
   const session = useSession()
   const userId = session.data?.user.id
-  const { OFFER_CONSTANT } = useOfferConstant();
+  const { OFFER_CONSTANT } = useOfferConstant()
   const [isOpen, setIsOpen] = useState(false)
   const methods = useForm<CreateOfferType>({
-    resolver: zodResolver(createOfferSchema)
+    resolver: zodResolver(createOfferSchema),
   })
 
   const { mutateAsync: createChat, isPending: isCreatingChat } = useMutation({
-    mutationFn: ChatService.create
+    mutationFn: ChatService.create,
   })
 
   const { mutateAsync: createOffer, isPending: isCreatingOffer } = useMutation({
-    mutationFn: async (data: { amount: number, chatId: string }) => {
+    mutationFn: async (data: { amount: number; chatId: string }) => {
       await OfferService.create(data.chatId, data)
-    }
+    },
   })
 
   async function createChatAndOffer(data: { amount: number }) {
@@ -43,29 +46,45 @@ export function OfferModal(product: Product) {
   }
 
   if (isCreatingChat) {
-    return <div className="w-full h-full flex items-center justify-center">
-      <span className="w-full">Criando chat...</span>
-    </div>
+    return (
+      <div className="flex size-full items-center justify-center">
+        <span className="w-full">Criando chat...</span>
+      </div>
+    )
   }
 
   if (isCreatingOffer) {
-    return <div className="w-full h-full flex items-center justify-center">
-      <span className="w-full">Criando oferta...</span>
-    </div>
+    return (
+      <div className="flex size-full items-center justify-center">
+        <span className="w-full">Criando oferta...</span>
+      </div>
+    )
   }
 
-  return <Dialog onOpenChange={setIsOpen} open={isOpen} modal>
-    <DialogTrigger asChild>
-      <Button onClick={async () => {
-        setIsOpen(true)
-      }} className="w-full">Realizar oferta</Button>
-    </DialogTrigger>
-    <DialogContent>
-      <FormRender constant={OFFER_CONSTANT} form={methods} onSubmit={createChatAndOffer} className="w-full">
-        <div className="flex mt-2 items-center justify-end">
-          <Button className="self-end">Confirmar</Button>
-        </div>
-      </FormRender>
-    </DialogContent>
-  </Dialog>
-} 
+  return (
+    <Dialog onOpenChange={setIsOpen} open={isOpen} modal>
+      <DialogTrigger asChild>
+        <Button
+          onClick={async () => {
+            setIsOpen(true)
+          }}
+          className="w-full"
+        >
+          Realizar oferta
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <FormRender
+          constant={OFFER_CONSTANT}
+          form={methods}
+          onSubmit={createChatAndOffer}
+          className="w-full"
+        >
+          <div className="mt-2 flex items-center justify-end">
+            <Button className="self-end">Confirmar</Button>
+          </div>
+        </FormRender>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/components/routes/product-details/offers-session.tsx
+++ src/components/routes/product-details/offers-session.tsx
@@ -5,25 +5,39 @@ import { Product } from '../../../@types/product'
 import { OfferService } from '../../../services/offer-service'
 import { OfferModal } from './offer-modal'
 import { OfferCard } from './offer-sender-session'
+import { motion } from 'framer-motion'
+import { useSession } from 'next-auth/react'
 
-export function OffersSession(data: { product: Product }) {
+export function OffersSession({ product }: { product: Product }) {
   const { data: offers } = useQuery({
-    queryKey: ['offers', data.product.id],
+    queryKey: ['offers', product.id],
     queryFn: OfferService.getManyByUserId,
   })
-  return <div className='flex flex-col gap-8'>
-    {offers && offers.length > 0
-      ? offers.map((item) =>
-        <OfferCard offer={item} />
-      )
-      : <Card>
-        <CardContent className='flex flex-col gap-8 justify-center items-center'>
-          <h2 className='text-2xl font-bold'>Nenhuma oferta realizada</h2>
-        </CardContent>
-      </Card>
-    }
-    <OfferModal {...data.product} />
+  const { data } = useSession()
+  const isProductAuthor = product.authorId === data?.user.id
+  return (
+    <motion.div
+      animate={{
+        opacity: [0, 1],
+        translateY: [-10, 0],
+        transition: { duration: 0.5, ease: 'easeInOut', delay: 0.7 },
+      }}
+      className="flex h-full w-2/5 flex-col gap-5"
+    >
+      <h2 className="text-3xl font-bold">Ofertas:</h2>
 
-  </div>
-
+      <div className="flex flex-col gap-8">
+        {offers && offers.length > 0 ? (
+          offers.map((item, index) => <OfferCard key={index} offer={item} />)
+        ) : (
+          <Card>
+            <CardContent className="flex flex-col items-center justify-center gap-8">
+              <h2 className="text-2xl font-bold">Nenhuma oferta realizada</h2>
+            </CardContent>
+          </Card>
+        )}
+        {!isProductAuthor && <OfferModal {...product} />}
+      </div>
+    </motion.div>
+  )
 }
