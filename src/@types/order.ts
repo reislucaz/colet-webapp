fc60d8f2520a6e7aad2acf9489bccc574cdfa3f3
Gustavo Bertonsin <gustavo.bertonsin@kingspanisoeste.com.br>
2025-09-30 13:32:49 -0300

    feat: add order type

---
+++ src/@types/order.ts
@@ -0,0 +1,15 @@
+import { Offer } from "./offer"
+import { Product } from "./product"
+import { User } from "./users"
+
+export interface Order {
+  id: string
+  amount: number
+  status: string
+  seller: User
+  purchaser: User
+  product: Product
+  offer: Offer
+  createdAt: string
+  updatedAt: string
+}
