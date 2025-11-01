c62576aa0e5bfee2fbe6e1a493b073b61317a50a
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2025-11-01 11:56:26 -0300

    feat: adiciona utils

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -7,3 +7,23 @@ export const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
 export function formattedDateFNS(date: Date) {
   return date && format(new Date(date), 'dd/MM/yyyy')
 }
+
+export const formatDate = (isoString: string): string => {
+  return new Date(isoString).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+}
+
+export const formatTimestamp = (timestamp: number): string => {
+  return new Date(timestamp * 1000).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+}
